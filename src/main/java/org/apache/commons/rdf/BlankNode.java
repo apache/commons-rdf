c330c39fee0a6995ba42079b67cb40bf31d7cbf5
Andy Seaborne <andy.seaborne@gmail.com>
2014-06-10 10:17:27 +0100

    Possible operation for BlankNode. Discuss.

--- src/main/java/org/apache/commons/rdf/BlankNode.java
+++ src/main/java/org/apache/commons/rdf/BlankNode.java
@@ -1,4 +1,10 @@
 package org.apache.commons.rdf;
 
 public interface BlankNode extends Resource {
+    /** Return a label for the blank node.
+     * This is not a serialization/syntax label.
+     * It should be uniquely identifying within this system
+     * but has no uniqueness guarantees other than that.
+     */
+	Object getLabel() ;
 }
