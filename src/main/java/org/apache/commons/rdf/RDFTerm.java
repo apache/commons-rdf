d7e5c188f597ec7e2759567843c67cc1823a0835
Peter Ansell <p_ansell@yahoo.com>
2014-06-11 14:17:33 +1000

    javadoc RDFTerm

--- src/main/java/org/apache/commons/rdf/RDFTerm.java
+++ src/main/java/org/apache/commons/rdf/RDFTerm.java
@@ -1,15 +1,21 @@
 package org.apache.commons.rdf;
 
 /**
- * RDF Term
+ * An <a href=
+ * "http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/#dfn-rdf-term"
+ * >RDF-1.1 Term</a>.
+ * 
+ * @see <a href=
+ *      "http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/#dfn-rdf-term"
+ *      >RDF-1.1 Term</a>
  */
 public interface RDFTerm {
 
-    /**
-     * Return the term representation (TBD)
-     *
-     * @return term representation
-     */
-    String toString();
+	/**
+	 * Return the term representation (TBD)
+	 *
+	 * @return term representation
+	 */
+	String toString();
 
 }
