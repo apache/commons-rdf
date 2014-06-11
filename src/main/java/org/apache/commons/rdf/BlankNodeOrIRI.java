c993024ca8a713baa14cf5d0e0cdd5d277ebd295
Peter Ansell <p_ansell@yahoo.com>
2014-06-11 14:20:38 +1000

    javadoc BlankNodeOrIRI

--- src/main/java/org/apache/commons/rdf/BlankNodeOrIRI.java
+++ src/main/java/org/apache/commons/rdf/BlankNodeOrIRI.java
@@ -1,5 +1,13 @@
 package org.apache.commons.rdf;
 
-public interface BlankNodeOrIRI extends RDFTerm 
-{ 
+/**
+ * An abstraction used to represent cases where either a {@link BlankNode} or an
+ * {@link IRI} can be used interchangeably.<br>
+ * Examples of its use include <a href=
+ * "http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/#dfn-subject" >RDF-1.1
+ * Triple subjects</a> and <a href=
+ * "http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/#dfn-named-graph"
+ * >RDF-1.1 Named Graphs</a>.
+ */
+public interface BlankNodeOrIRI extends RDFTerm {
 }
