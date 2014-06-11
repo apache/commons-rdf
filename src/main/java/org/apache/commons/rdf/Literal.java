a72ae1fe3fa0bee57df16cd6a96d9c2f20f2063a
Peter Ansell <p_ansell@yahoo.com>
2014-06-11 13:53:44 +1000

    javadoc Literal

--- src/main/java/org/apache/commons/rdf/Literal.java
+++ src/main/java/org/apache/commons/rdf/Literal.java
@@ -1,9 +1,51 @@
 package org.apache.commons.rdf;
 
+import java.util.Optional;
+
+/**
+ * An RDF-1.1 Literal, as defined by <a href=
+ * "http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/#section-Graph-Literal"
+ * >RDF-1.1 Concepts and Abstract Syntax</a>, a W3C Recommendation published on
+ * 25 February 2014
+ */
 public interface Literal extends RDFTerm {
-	String getLexicalForm() ;
+	
+	/**
+	 * The lexical form of this literal, represented by a <a
+	 * href="http://www.unicode.org/versions/latest/">Unicode string</a>, which
+	 * SHOULD be in <a href="http://www.unicode.org/reports/tr15/">Normal Form
+	 * C</a>.
+	 * 
+	 * @return The lexical form of this literal.
+	 * @see <a
+	 *      href="http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/#dfn-lexical-form">RDF-1.1
+	 *      Literal lexical form</a>
+	 */
+	String getLexicalForm();
 
-	IRI getDatatype() ;
+	/**
+	 * The IRI identifying the datatype that determines how the lexical form
+	 * maps to a literal value.
+	 * 
+	 * @return The datatype IRI for this literal.
+	 * @see <a
+	 *      href="http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/#dfn-datatype-iri">RDF-1.1
+	 *      Literal datatype IRI</a>
+	 */
+	IRI getDatatype();
 
-    Optional<String> getLanguageTag() ;
+	/**
+	 * If and only if the datatype IRI is <a
+	 * href="http://www.w3.org/1999/02/22-rdf-syntax-ns#langString"
+	 * >http://www.w3.org/1999/02/22-rdf-syntax-ns#langString</a>, the language
+	 * tag is a non-empty language tag as defined by <a
+	 * href="http://tools.ietf.org/html/bcp47">BCP47</a>.
+	 * 
+	 * @return The {@link Optional} language tag for this literal.
+	 * @see <a
+	 *      href="http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/#dfn-language-tag">RDF-1.1
+	 *      Literal language tag</a>
+	 */
+	Optional<String> getLanguageTag();
+	
 }
