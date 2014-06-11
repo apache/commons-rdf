80938ed1206327aa3c108bcf9f984c6e6bd72016
Peter Ansell <p_ansell@yahoo.com>
2014-06-11 14:31:06 +1000

    javadoc IRI

--- src/main/java/org/apache/commons/rdf/IRI.java
+++ src/main/java/org/apache/commons/rdf/IRI.java
@@ -1,4 +1,10 @@
 package org.apache.commons.rdf;
 
+/**
+ * An <a href= "http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/#dfn-iri"
+ * >RDF-1.1 IRI</a>, as defined by <a href=
+ * "http://www.w3.org/TR/2014/REC-rdf11-concepts-20140225/" >RDF-1.1 Concepts
+ * and Abstract Syntax</a>, a W3C Recommendation published on 25 February 2014.<br>
+ */
 public interface IRI extends BlankNodeOrIRI {
 }
