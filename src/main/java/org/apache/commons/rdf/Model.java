45150edda758b3e793ac98c2bf8442f057b7cbe3
Sergio Fernández <wikier@apache.org>
2014-06-12 12:43:27 +0200

    added initial desing of a in-memory model

---
+++ src/main/java/org/apache/commons/rdf/Model.java
@@ -0,0 +1,31 @@
+package org.apache.commons.rdf;
+
+import java.util.Collection;
+
+/**
+ * In-memory Model
+ *
+ */
+public interface Model {
+
+    boolean add(Triple triple);
+
+    boolean add(Resource subject, IRI predicate, RDFTerm object);
+
+    boolean add(Collection<? extends Triple> triples);
+
+    boolean remove(Triple triple);
+
+    boolean clear();
+
+    int size();
+
+    Collection<? extends Triple> getTriples();
+
+    Collection<? extends Resource> getSubjects();
+
+    Collection<? extends IRI> getPredicates();
+
+    Collection<? extends RDFTerm> getObjects();
+
+}
