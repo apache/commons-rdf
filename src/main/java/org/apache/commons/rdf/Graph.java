34d6388946d385c60fd66dd6f0766546caf56675
Sergio Fernández <wikier@apache.org>
2014-06-13 11:28:03 +0200

    renamed model to graph, as suggested by @ansell, to match RDF-1.1 terminology

--- src/main/java/org/apache/commons/rdf/Graph.java
+++ src/main/java/org/apache/commons/rdf/Graph.java
@@ -4,13 +4,16 @@ import java.util.Collection;
 import java.util.Iterator;
 
 /**
- * Model, a collection of RDF triples
- *
+ * An <a href="http://www.w3.org/TR/rdf11-concepts/#section-rdf-graph">
+ * RDF 1.1 Graph</a>, a set of RDF triples, as defined by <a
+ * href="http://www.w3.org/TR/rdf11-concepts/" >RDF-1.1 Concepts
+ * and Abstract Syntax</a>, a W3C Recommendation published on 25
+ * February 2014.
  */
-public interface Model {
+public interface Graph {
 
     /**
-     * Add a triple to the model
+     * Add a triple to the graph
      *
      * @param triple triple to add
      * @return added or not
@@ -18,7 +21,7 @@ public interface Model {
     boolean add(Triple triple);
 
     /**
-     * Add a triple to the model
+     * Add a triple to the graph
      *
      * @param subject subject of the triple
      * @param predicate predicate of the triple
@@ -28,7 +31,7 @@ public interface Model {
     boolean add(Resource subject, IRI predicate, RDFTerm object);
 
     /**
-     * Add a collection of triples to the model
+     * Add a collection of triples to the graph
      *
      * @param triples triples to add
      * @return added all or not
@@ -36,7 +39,7 @@ public interface Model {
     boolean add(Collection<? extends Triple> triples);
 
     /**
-     * Remove a concrete triple from the model
+     * Remove a concrete triple from the graph
      *
      * @param triple triple to remove
      * @return removed or not
@@ -44,7 +47,7 @@ public interface Model {
     boolean remove(Triple triple);
 
     /**
-     * emove a concrete pattern of triples from the model
+     * emove a concrete pattern of triples from the graph
      *
      * @param subject subject (null is a wildcard)
      * @param predicate  predicate (null is a wildcard)
@@ -54,28 +57,28 @@ public interface Model {
     boolean remove(Resource subject, IRI predicate, RDFTerm object);
 
     /**
-     * Clear the model
+     * Clear the graph
      *
      * @return clearer or not
      */
     boolean clear();
 
     /**
-     * Number of triples contained by the model
+     * Number of triples contained by the graph
      *
      * @return size
      */
     long size();
 
     /**
-     * Get all triples contained by the model
+     * Get all triples contained by the graph
      *
      * @return iterator over all triples
      */
     Iterator<? extends Triple> getTriples();
 
     /**
-     * Get all triples contained by the model matched with the pattern
+     * Get all triples contained by the graph matched with the pattern
      *
      * @param subject subject (null is a wildcard)
      * @param predicate  predicate (null is a wildcard)
