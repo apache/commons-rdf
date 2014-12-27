dbb363b7211ebb652cbb661c65cc859dec590dc2
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-12-27 03:03:51 -0600

    test Graph.toString, size, contains, remove

--- src/test/java/com/github/commonsrdf/api/AbstractGraphTest.java
+++ src/test/java/com/github/commonsrdf/api/AbstractGraphTest.java
@@ -3,6 +3,8 @@ package com.github.commonsrdf.api;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public abstract class AbstractGraphTest {
 
 	private RDFTermFactory factory;
@@ -27,6 +29,8 @@ public abstract class AbstractGraphTest {
 	@Before
 	public void createGraph() {
 		graph = factory.createGraph();
+		assertEquals(0, graph.size());
+		
 		alice = factory.createIRI("http://example.com/alice");
 		bob = factory.createIRI("http://example.com/bob");
 		name = factory.createIRI("http://xmlns.com/foaf/0.1/name");
@@ -34,23 +38,61 @@ public abstract class AbstractGraphTest {
 		member = factory.createIRI("http://xmlns.com/foaf/0.1/member");
 		org1 = factory.createBlankNode("org1");
 		org2 = factory.createBlankNode("org2");
-		aliceLit = factory.createLiteral("Alice");
-		bobLit = factory.createLiteral("Bob", "en-US");
 		
-		graph.add(alice, name, aliceLit);
+		graph.add(alice, name, factory.createLiteral("Alice"));
 		graph.add(alice, knows, bob);
 		graph.add(alice, member, org1);
-		graph.add(bob, name, bobLit);
-		graph.add(bob, knows, alice);
-		graph.add(bob, member, org1);
-		graph.add(bob, member, org2);
+		// and for Bob we'll try as Triples
+		graph.add(factory.createTriple(bob, name, factory.createLiteral("Bob", "en-US"))); 
+		graph.add(factory.createTriple(bob, member, org1));
+		graph.add(factory.createTriple(bob, member, org2));
+		
+		graph.add(org1, name, factory.createLiteral("The Secret Club"));
+		graph.add(org1, name, factory.createLiteral("A company"));
+	}
+	
+	
+	@Test
+	public void graphToString() {
+		
 		System.out.println(graph);
+		assertTrue(graph.toString().startsWith(
+				"<http://example.com/alice> <http://xmlns.com/foaf/0.1/name> \"Alice\" ."));
+		assertTrue(graph.toString().endsWith(
+				"_:org1 <http://xmlns.com/foaf/0.1/name> \"A company\" ."));
+		
 	}
 	
 	@Test
-	public void testName() throws Exception {
-		
-		
+	public void size() throws Exception {
+		assertEquals(8, graph.size());
+	}
+	
+	@Test
+	public void contains() throws Exception {
+		assertTrue(graph.contains(alice, knows, bob));
+		assertFalse(graph.contains(bob, knows, alice)); // or so he claims..
+	}
+	
+	@Test
+	public void remove() throws Exception {
+		graph.remove(alice, knows, bob);
+		assertEquals(7, graph.size());	
+		graph.remove(alice, knows, bob);
+		assertEquals(7, graph.size());
+		graph.add(alice, knows, bob);
+		graph.add(alice, knows, bob);
+		graph.add(alice, knows, bob);
+		// Undetermined size at this point
+		//assertEquals(8, graph.size());
+		// but at least after remove they should all be gone
+		graph.remove(alice, knows, bob);
+		assertEquals(7, graph.size());
+	}
+	
+	@Test
+	public void something() throws Exception {
+	}		
 	}
 	
 	
