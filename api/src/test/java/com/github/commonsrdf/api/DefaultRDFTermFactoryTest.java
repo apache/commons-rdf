2bfe31612e963b6d2b5e50f13fd2e518ffcf0525
Peter Ansell <p_ansell@yahoo.com>
2015-03-11 09:17:02 +1100

    move DefaultRDFTermFactoryTest to the api module as it doesn't rely on simple
    also, jacoco/coveralls doesn't currently recognise the test coverage if
    it is run in the simple module

--- api/src/test/java/com/github/commonsrdf/api/DefaultRDFTermFactoryTest.java
+++ api/src/test/java/com/github/commonsrdf/api/DefaultRDFTermFactoryTest.java
@@ -11,9 +11,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.commonsrdf.simple;
+package com.github.commonsrdf.api;
 
-import com.github.commonsrdf.api.AbstractRDFTermFactoryTest;
 import com.github.commonsrdf.api.RDFTermFactory;
 
 /**
