683a2bbdf7539772950d4eaaa0018745eaf6cc2d
Stian Soiland-Reyes <stain@apache.org>
2016-04-08 16:21:49 +0100

    Added DatasetImpl, QuadImpl

---
+++ simple/src/main/java/org/apache/commons/rdf/simple/DatasetImpl.java
@@ -0,0 +1,223 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.rdf.simple;
+
+import org.apache.commons.rdf.api.*;
+import org.apache.commons.rdf.simple.SimpleRDFTermFactory.SimpleRDFTerm;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * A simple, memory-based implementation of Dataset.
+ * <p>
+ * {@link Quad}s in the graph are kept in a {@link Set}.
+ * <p>
+ * All Stream operations are performed using parallel and unordered directives.
+ */
+final class DatasetImpl implements Dataset {
+	
+    private static final int TO_STRING_MAX = 10;
+    private final Set<Quad> quads = new HashSet<Quad>();
+    private final SimpleRDFTermFactory factory;
+
+    DatasetImpl(SimpleRDFTermFactory simpleRDFTermFactory) {
+        this.factory = simpleRDFTermFactory;
+    }
+
+    @Override
+    public void add(BlankNodeOrIRI subject, IRI predicate, RDFTerm object) {
+    	// TODO: Should this method be promoted as a default method in Dataset?     	    	    	
+    	add(null, subject, predicate, object);
+    }
+    
+	@Override
+	public void add(BlankNodeOrIRI graphName, BlankNodeOrIRI subject, IRI predicate, RDFTerm object) {
+		BlankNodeOrIRI newGraphName = (BlankNodeOrIRI) internallyMap(graphName);
+		BlankNodeOrIRI newSubject = (BlankNodeOrIRI) internallyMap(subject);
+        IRI newPredicate = (IRI) internallyMap(predicate);
+        RDFTerm newObject = internallyMap(object);
+        Quad result = factory.createQuad(newGraphName, newSubject, newPredicate, newObject);
+        quads.add(result);
+	}    
+
+    @Override
+    public void add(Quad quad) {    	
+    	BlankNodeOrIRI newGraph = (BlankNodeOrIRI) internallyMap(quad.getGraphName().orElse(null));
+    	BlankNodeOrIRI newSubject = (BlankNodeOrIRI) internallyMap(quad
+                .getSubject());        
+        IRI newPredicate = (IRI) internallyMap(quad.getPredicate());
+        RDFTerm newObject = internallyMap(quad.getObject());
+        // Check if any of the object references changed during the mapping, to
+        // avoid creating a new Quad object if possible
+        if (newGraph == quad.getGraphName().orElse(null) 
+        	&& newSubject == quad.getSubject()
+            && newPredicate == quad.getPredicate()
+            && newObject == quad.getObject()) {
+            quads.add(quad);
+        } else {
+        	// Make a new Quad with our mapped instances
+            Quad result = factory.createQuad(newGraph, newSubject, newPredicate,
+                    newObject);
+            quads.add(result);
+        }
+    }
+
+    private <T extends RDFTerm> RDFTerm internallyMap(T object) {
+    	if (object == null || object instanceof SimpleRDFTerm) {
+    		return object;
+    	}    	
+        if (object instanceof BlankNode && !(object instanceof BlankNodeImpl)) {
+            BlankNode blankNode = (BlankNode) object;
+            // This guarantees that adding the same BlankNode multiple times to
+            // this graph will generate a local object that is mapped to an
+            // equivalent object, based on the code in the package private
+            // BlankNodeImpl class
+            return factory.createBlankNode(blankNode.uniqueReference());
+        } else if (object instanceof IRI && !(object instanceof IRIImpl)) {
+            IRI iri = (IRI) object;
+            return factory.createIRI(iri.getIRIString());
+        } else if (object instanceof Literal
+                && !(object instanceof LiteralImpl)) {
+            Literal literal = (Literal) object;
+            if (literal.getLanguageTag().isPresent()) {
+                return factory.createLiteral(literal.getLexicalForm(), literal
+                        .getLanguageTag().get());
+            } else {
+                return factory.createLiteral(literal.getLexicalForm(),
+                        (IRI) internallyMap(literal.getDatatype()));
+            }
+        } else {
+        	throw new IllegalArgumentException("Not a BlankNode, IRI or Literal: " + object);
+        }
+    }
+
+    @Override
+    public void clear() {
+        quads.clear();
+    }
+
+    @Override
+    public boolean contains(BlankNodeOrIRI subject, IRI predicate,
+                            RDFTerm object) {
+    	// TODO: Should this method be promoted as a default method in Dataset?     	    	
+        return getQuads(Optional.empty(), subject, predicate, object).findAny().isPresent();
+    }
+
+	@Override
+	public boolean contains(Optional<BlankNodeOrIRI> graphName, BlankNodeOrIRI subject, IRI predicate, RDFTerm object) {
+		return getQuads(graphName, subject, predicate, object).findAny().isPresent();
+	}
+    
+    @Override
+    public boolean contains(Quad quad) {
+        return quads.contains(Objects.requireNonNull(quad));
+    }
+    
+	@Override
+	public Stream<Quad> getQuads() {
+        return quads.parallelStream().unordered();
+	}
+
+	@Override
+	public Stream<Quad> getQuads(Optional<BlankNodeOrIRI> graphName, BlankNodeOrIRI subject, IRI predicate,
+			RDFTerm object) {
+		final Optional<BlankNodeOrIRI> newGraphName = graphName.map(g -> (BlankNodeOrIRI)internallyMap(g));
+        final BlankNodeOrIRI newSubject = (BlankNodeOrIRI) internallyMap(subject);
+        final IRI newPredicate = (IRI) internallyMap(predicate);
+        final RDFTerm newObject = internallyMap(object);
+
+        return getQuads(t -> {
+            if (newGraphName != null && !t.getGraphName().equals(newGraphName)) {
+            	// This would check Optional.empty() == Optional.empty()
+                return false;
+            }        	
+            if (subject != null && !t.getSubject().equals(newSubject)) {
+                return false;
+            }
+            if (predicate != null && !t.getPredicate().equals(newPredicate)) {
+                return false;
+            }
+            if (object != null && !t.getObject().equals(newObject)) {
+                return false;
+            }
+            return true;
+        });
+	}    
+
+    @Override
+    public Stream<Triple> getTriples() {
+    	// TODO: Should this method be promoted as a default method in Dataset?     	
+    	return getQuads(Optional.empty(), null, null, null).map(Quad::asTriple);
+    }
+
+    @Override
+    public Stream<Triple> getTriples(final BlankNodeOrIRI subject,
+                                     final IRI predicate, final RDFTerm object) {
+    	// TODO: Should this method be promoted as a default method in Dataset?     	
+    	return getQuads(Optional.empty(), subject, predicate, object).map(Quad::asTriple);
+    }
+
+    private Stream<Quad> getQuads(final Predicate<Quad> filter) {
+        return getQuads().filter(filter);
+    }
+
+    @Override
+    public void remove(BlankNodeOrIRI subject, IRI predicate, RDFTerm object) {
+    	// TODO: Should this method be promoted as a default method in Dataset?     	    	
+    	remove(Optional.empty(), subject, predicate, object);
+    }
+
+	@Override
+	public void remove(Optional<BlankNodeOrIRI> graphName, BlankNodeOrIRI subject, IRI predicate, RDFTerm object) {
+        Stream<Quad> toRemove = getQuads(graphName, subject, predicate, object);
+        for (Quad t : toRemove.collect(Collectors.toList())) {
+            // Avoid ConcurrentModificationException in ArrayList
+            remove(t);
+        }
+	}
+
+
+    @Override
+    public void remove(Quad quad) {
+        quads.remove(Objects.requireNonNull(quad));
+    }
+
+    @Override
+    public long size() {
+        return quads.size();
+    }
+
+    @Override
+    public String toString() {
+        String s = getTriples().limit(TO_STRING_MAX).map(Object::toString)
+                .collect(Collectors.joining("\n"));
+        if (size() > TO_STRING_MAX) {
+            return s + "\n# ... +" + (size() - TO_STRING_MAX) + " more";
+        } else {
+            return s;
+        }
+    }
+
+
+}
