8ef35e6c0bec86207de97707701ae5add7bd09ce
Stian Soiland-Reyes <stain@apache.org>
2016-06-20 12:28:42 +0100

    javadoc on rdf4j implementation

--- rdf4j/src/main/java/org/apache/commons/rdf/rdf4j/package-info.java
+++ rdf4j/src/main/java/org/apache/commons/rdf/rdf4j/package-info.java
@@ -18,9 +18,36 @@
 /**
  * Commons RDF integration with <a href="http://rdf4j.org/">RDF4J</a>.
  * <p>
+ * Use the {@link org.apache.commons.rdf.rdf4j.RDF4JTermFactory} to convert
+ * between Commons RDF and RDF4J types, for instance
+ * {@link org.apache.commons.rdf.rdf4j.RDF4JTermFactory#asQuad(org.eclipse.rdf4j.model.Statement)}
+ * converts a RDF4J {@link org.eclipse.rdf4j.model.Statement} to a
+ * {@link org.apache.commons.rdf.api.Quad}. Converted RDF terms implement the
+ * {@link org.apache.commons.rdf.rdf4j.RDF4JTerm} interface, and converted
+ * statements the {@link org.apache.commons.rdf.rdf4j.RDF4JTripleLike}
+ * interface, which provide convenience access to the underlying RDF4J
+ * implementations.
+ * <p>
+ * RDF4J {@link org.eclipse.rdf4j.model.Model}s and
+ * {@link org.eclipse.rdf4j.repository.Repository} instances can be adapted to
+ * Commons RDF {@link org.apache.commons.rdf.api.Graph} and
+ * {@link org.apache.commons.rdf.api.Dataset}, e.g. using
+ * {@link org.apache.commons.rdf.rdf4j.RDF4JTermFactory#asRDFTermGraph(org.eclipse.rdf4j.model.Model)}
+ * or
+ * {@link org.apache.commons.rdf.rdf4j.RDF4JTermFactory#asRDFTermDataset(org.eclipse.rdf4j.repository.Repository)}.
+ * The returned adapted graph/dataset is directly mapped, so changes are
+ * propagated both ways. For convenience, the marker interface
+ * {@link org.apache.commons.rdf.rdf4j.RDF4JGraph} and
+ * {@link org.apache.commons.rdf.rdf4j.RDF4JDataset} provide access to the
+ * underlying RDF4J implementations.
+ * <p>
+ * The {@link org.apache.commons.rdf.rdf4j.RDF4JParserBuilder} can be used to
+ * parse RDF files using RDF4j. It should be most efficient if used with
+ * {@link org.apache.commons.rdf.rdf4j.RDF4JParserBuilder#target(org.apache.commons.rdf.api.Dataset)}
+ * and an adapted {@link org.apache.commons.rdf.rdf4j.RDF4JDataset}, or
+ * {@link org.apache.commons.rdf.rdf4j.RDF4JParserBuilder#target(org.apache.commons.rdf.api.Graph)}
+ * and a an adapted {@link org.apache.commons.rdf.rdf4j.RDF4JGraph}
  * 
  *
  */
 package org.apache.commons.rdf.rdf4j;
-
-
