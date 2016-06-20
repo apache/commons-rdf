9c66ae0834c0908eed0035fa258f0ea38e060259
Stian Soiland-Reyes <stain@apache.org>
2016-06-20 12:29:13 +0100

    Move RDF4JParserBuilder up from rdfj.impl

--- rdf4j/src/main/java/org/apache/commons/rdf/rdf4j/RDF4JParserBuilder.java
+++ rdf4j/src/main/java/org/apache/commons/rdf/rdf4j/RDF4JParserBuilder.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.rdf.rdf4j.impl;
+package org.apache.commons.rdf.rdf4j;
 
 import java.io.IOException;
 import java.nio.file.Path;
