47734d7eb054418e6da8be0702fa87f01baae75f
Stian Soiland-Reyes <stain@apache.org>
2016-10-28 14:13:01 +0100

    TripleLike/QuadLike NOT required to thread-safe or immutable

--- api/src/main/java/org/apache/commons/rdf/api/TripleLike.java
+++ api/src/main/java/org/apache/commons/rdf/api/TripleLike.java
@@ -24,8 +24,9 @@ package org.apache.commons.rdf.api;
  * A TripleLike statement has at least a {@link #getSubject()},
  * {@link #getPredicate()} and {@link #getObject()}, but unlike a {@link Triple}
  * does not have a formalised {@link Triple#equals(Object)} or
- * {@link Triple#hashCode()} semantics. This interfaced can also be used for
- * <em>generalised triples</em> (e.g. a {@link BlankNode} as predicate).
+ * {@link Triple#hashCode()} semantics and is not required to be
+ * <em>immutable</em> or <em>thread-safe</em>. This interfaced can also be used
+ * for <em>generalised triples</em> (e.g. a {@link BlankNode} as predicate).
  * <p>
  * Implementations should specialise which specific {@link RDFTerm} types they
  * return by overriding {@link #getSubject()}, {@link #getPredicate()} and
